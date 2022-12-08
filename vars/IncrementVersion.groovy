f7c26cf62bd01a8f4a48c733508ef11a495a2314
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2022-12-08 17:01:30 +0100

    Update IncrementVersion.groovy

--- vars/IncrementVersion.groovy
+++ vars/IncrementVersion.groovy
@@ -1,4 +1,5 @@
-def versionInc() {
+#!/usr/bin/env groovy
+def call () {
      echo 'increment app version ../'
      sh 'mvn build-helper:parse-version versions:set \
        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
