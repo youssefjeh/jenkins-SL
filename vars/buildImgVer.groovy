e73728f270906c5e2a1b20bdbe08633b208aca72
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2022-12-08 17:19:02 +0100

    Update buildImgVer.groovy

--- vars/buildImgVer.groovy
+++ vars/buildImgVer.groovy
@@ -3,9 +3,9 @@
 def call() {
     echo 'building docker image..'
     withCredentials([usernamePassword(credentialsId:'dockerhub-rep', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t youssefjeh/my-app:$IMAGE_NAME ."
-        sh "docker tag youssefjeh/my-app:$IMAGE_NAME dockerysf/my-app:$IMAGE_NAME"
+        sh "docker build -t youssefjeh/my-app:${IMAGE_NAME} ."
+        sh "docker tag youssefjeh/my-app:${IMAGE_NAME} dockerysf/my-app:${IMAGE_NAME}"
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push dockerysf/my-app:$IMAGE_NAME"
+        sh "docker push dockerysf/my-app:${IMAGE_NAME}"
     }
 }
