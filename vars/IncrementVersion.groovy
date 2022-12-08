e50cd4737d171fbf3ef48d0d980828da642889b2
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2022-12-08 16:58:35 +0100

    Create IncrementVersion.groovy

---
+++ vars/IncrementVersion.groovy
@@ -0,0 +1,9 @@
+def versionInc() {
+     echo 'increment app version ../'
+     sh 'mvn build-helper:parse-version versions:set \
+       -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+       virsions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+}
