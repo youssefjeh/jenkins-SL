64e013d0f3166b3a34dd9ec33aded1e60d776ce1
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2022-12-24 16:30:36 +0100

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -2,7 +2,7 @@
 
 def call() {
     echo 'azure login and push the docker image to acr hub ...'
-    withCredentials([usernamePassword(credentialsId:'azure_acr_credentials-', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId:'azure_acr_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker tag youssefjeh/my-app:v6.0 javamvn.azurecr.io/my-app:v6.0'
         sh "echo $PASS | docker login -u $USER --password-stdin javamvn.azurecr.io"
         sh 'docker image push javamvn.azurecr.io/my-app:v6.0'
