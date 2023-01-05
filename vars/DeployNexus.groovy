a71845e2ea82eda24e02936ed0eaab5258cc74e3
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2023-01-05 11:37:14 +0100

    Update DeployNexus.groovy

--- vars/DeployNexus.groovy
+++ vars/DeployNexus.groovy
@@ -2,9 +2,9 @@
 
 def call() {
     echo 'deploy to Nexus..'
-    def pom = readMavenPom(https://github.com/youssefjeh/Maven-deploy-to-Nexus-using-Jenkins/blob/main/pom.xml);
+    def pom = readMavenPom file: "https://github.com/youssefjeh/Maven-deploy-to-Nexus-using-Jenkins/blob/main/pom.xml";
    // pom = readMavenPom file: "https://github.com/youssefjeh/Maven-deploy-to-Nexus-using-Jenkins/blob/main/pom.xml";
-    def filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
     echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
     artifactPath = filesByGlob[0].path;
     artifactExists = fileExists artifactPath;
